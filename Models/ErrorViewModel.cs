257de052fc761f67e080b7a33a2a752e76216135
ulc <ulisses.cavalcante@altitude.com>
2019-02-04 22:14:48 -0200

    change to mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnetmvc002.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
